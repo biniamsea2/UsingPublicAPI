b49730b28f946e5b35bafcae7bf0275b672ded7d
Biniam <Biniamt23@yahoo.com>
2020-12-05 23:17:52 -0800

    working

--- PublicApiPractice/PublicApiPractice/Program.cs
+++ PublicApiPractice/PublicApiPractice/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace PublicApiPractice
 {
     class Program
     {
-        static void Main(string[] args)
+        // main method must be async of type Task (bring in threading.task namespace)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // use the httpclientfactory class to create a new instance of httpclient type
+            // httpclientfactory is used for creating HttpClient instances to be used in our application
+            // must install package (microsoft.aspnet.webapi.client)
+            var httpClient = HttpClientFactory.Create();
+            // api url saved in a string varibale 
+            string url = "https://anapioficeandfire.com/api/characters";
+            // our result from the api, with an await and getStringAsync
+            var data = await httpClient.GetStringAsync(url);
+
+            Console.WriteLine(data);
         }
     }
 }
